018a12594a657f03026a63dd395d80c65a8fbeb5
Kolya Korobochkin <me@korobochkin.com>
2017-01-08 20:23:59 +0300

    update phpunit config and create first tests

---
+++ tests/PluginTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Setka\Editor\Tests;
+
+use Korobochkin\BBPressPermalinksWithID\Plugin;
+
+class PluginTest extends \WP_UnitTestCase {
+
+	/**
+	 * @var \ReflectionClass Reflection instance for a \Setka\Editor\Plugin class.
+	 */
+	protected $pluginReflection;
+
+	/**
+	 * @var string A path to plugin root dir.
+	 */
+	protected $pluginRootPath;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->pluginReflection = new \ReflectionClass('Korobochkin\BBPressPermalinksWithID\Plugin');
+		$this->pluginRootPath = dirname(dirname($this->pluginReflection->getFileName()));
+	}
+
+	public function testPluginClassExists() {
+		$this->assertTrue(class_exists('Korobochkin\BBPressPermalinksWithID\Plugin'));
+	}
+
+	public function testPluginVersionsEquals() {
+		// plugin file
+		$plugin = $this->pluginRootPath . '/plugin.php';
+
+		// load info from WordPress metadata
+		$data = get_plugin_data($plugin, true, false);
+
+		$this->assertEquals(Plugin::VERSION, $data['Version']);
+	}
+
+	public function testPluginVersionsEqualsJSON() {
+		$packagePath = $this->pluginRootPath . '/package.json';
+
+		$this->assertFileExists($packagePath);
+		$this->assertTrue(is_readable($packagePath));
+
+		$package = fopen($packagePath, 'r');
+
+		$json = fread($package, filesize($packagePath));
+		$json = json_decode($json);
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+
+		$this->assertTrue(property_exists($json, 'version'));
+		$this->assertEquals(Plugin::VERSION, $json->version);
+	}
+}
