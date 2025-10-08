9ed64a279e76c487dddd273df39d90a0d1682cba
Nicolas Korobochkin <477498+korobochkin@users.noreply.github.com>
2025-10-08 19:48:16 +0400

    this is the way to install a custom version of bbpress from wp-cli image. because container uses volumes_from the user and group ids don't match from wordpress container and this requires using "user: 33:33". bad solution, i will try to improve this

---
+++ .github/tests/application/shells/install-wordpress.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+cd /var/www/html || exit 1
+
+wp core install \
+  --url=http://one.wordpress.test \
+  --title="Test Site" \
+  --admin_user=admin \
+  --admin_password=12345 \
+  --admin_email=admin@one.wordpress.test \
+  --locale=en_US \
+  --skip-email
+
+wp plugin install \
+  bbpress \
+  --version=2.6.0 \
+  --activate
