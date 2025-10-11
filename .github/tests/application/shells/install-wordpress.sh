d30ecc69fc06ae7c668a8e87c122eac6024d1ff6
Nicolas Korobochkin <477498+korobochkin@users.noreply.github.com>
2025-10-11 20:08:30 +0400

    Added conditions to install-wordpress.sh script

--- .github/tests/application/shells/install-wordpress.sh
+++ .github/tests/application/shells/install-wordpress.sh
@@ -4,17 +4,23 @@ set -Eeuo pipefail
 
 cd /var/www/html || exit 1
 
-wp core install \
-  --url="${TEST_SITE_HOME}" \
-  --title="${TEST_SITE_TITLE}" \
-  --admin_user="${TEST_SITE_ADMIN_LOGIN}" \
-  --admin_password="${TEST_SITE_ADMIN_PASSWORD}" \
-  --admin_email="${TEST_SITE_ADMIN_EMAIL}" \
-  --locale="${TEST_SITE_LOCALE}" \
-  --skip-email
+if ! wp core is-installed 2>/dev/null; then
+  wp core install \
+    --url="${TEST_SITE_HOME}" \
+    --title="${TEST_SITE_TITLE}" \
+    --admin_user="${TEST_SITE_ADMIN_LOGIN}" \
+    --admin_password="${TEST_SITE_ADMIN_PASSWORD}" \
+    --admin_email="${TEST_SITE_ADMIN_EMAIL}" \
+    --locale="${TEST_SITE_LOCALE}" \
+    --skip-email
+fi
 
-wp plugin activate \
-  bbpress
+if ! wp plugin is-active bbpress 2>/dev/null; then
+  wp plugin activate \
+    bbpress
+fi
 
-wp theme activate \
-  twentytwelve
+if ! wp theme is-active twentytwelve 2>/dev/null; then
+  wp theme activate \
+    twentytwelve
+fi
