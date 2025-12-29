45352283ed4ad50fccb3b5c6e572be4e9515d858
Nicolas Korobochkin <477498+korobochkin@users.noreply.github.com>
2025-12-29 18:39:09 +0400

    Class for testing topic edit page

---
+++ .github/tests/application/tests/TestCases/PluginIsNotActive/TopicEditTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\TestCases\PluginIsNotActive;
+
+use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\Abstracts\AbstractTopicEditTest;
+use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\DataProviders\ForumDataProvider;
+use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\Entities\Posts\Forum;
+use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\Entities\Posts\Topic;
+use PHPUnit\Framework\Attributes;
+
+/**
+ * @internal
+ */
+#[Attributes\CoversNothing]
+class TopicEditTest extends AbstractTopicEditTest
+{
+    #[Attributes\DependsOnClass(TopicPagedTest::class)]
+    #[Attributes\DataProviderExternal(ForumDataProvider::class, 'getTopics')]
+    public function testTopicEditAsGuest(Forum $forum, Topic $topic): void
+    {
+        $this->_testTopicEditAsGuest($this->browsers->guest, $forum, $topic);
+    }
+
+    #[Attributes\Depends('testTopicEditAsGuest')]
+    #[Attributes\DataProviderExternal(ForumDataProvider::class, 'getTopics')]
+    public function testTopicEditAsAdmin(Forum $forum, Topic $topic): void
+    {
+        $this->_testTopicEditAsAdmin($this->browsers->admin, $forum, $topic);
+    }
+}
