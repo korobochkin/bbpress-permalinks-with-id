f100d2fe4a8c61d038114893766bd562f4100fa6
Nicolas Korobochkin <477498+korobochkin@users.noreply.github.com>
2026-02-26 23:03:25 +0400

    Made a data provider from ForumsPage

--- .github/tests/application/tests/TestCases/PluginIsActive/ForumsPageTest.php
+++ .github/tests/application/tests/TestCases/PluginIsActive/ForumsPageTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\TestCases\PluginIsActive;
 
 use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\Abstracts\AbstractForumsPageTest;
+use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\DataProviders\ForumsPageDataProvider;
+use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\Entities\Posts\Page;
 use Korobochkin\BBPressPermalinksWithIdTestsApplication\Tests\TestCases\PluginIsNotActive\ActivatePluginTest;
 use PHPUnit\Framework\Attributes;
 
@@ -16,15 +18,17 @@ final class ForumsPageTest extends AbstractForumsPageTest
 {
     #[\Override]
     #[Attributes\DependsOnClass(ActivatePluginTest::class)]
-    public function testForumsPageAsGuest(): void
+    #[Attributes\DataProviderExternal(ForumsPageDataProvider::class, 'get')]
+    public function testForumsPageAsGuest(Page $forumsPage): void
     {
-        parent::testForumsPageAsGuest();
+        parent::testForumsPageAsGuest($forumsPage);
     }
 
     #[\Override]
     #[Attributes\DependsOnClass(ActivatePluginTest::class)]
-    public function testForumsPageAsAdmin(): void
+    #[Attributes\DataProviderExternal(ForumsPageDataProvider::class, 'get')]
+    public function testForumsPageAsAdmin(Page $forumsPage): void
     {
-        parent::testForumsPageAsAdmin();
+        parent::testForumsPageAsAdmin($forumsPage);
     }
 }
